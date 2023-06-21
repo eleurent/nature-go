9dac22da413019974c23559c751109c1bbeaf875
Edouard Leurent <eleurent@gmail.com>
2023-06-21 22:29:09 +0100

    Add more fields to Species

--- backend/nature_go/observation/models.py
+++ backend/nature_go/observation/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 class Species(models.Model):
     name = models.CharField(max_length=255)
+    commonNames = models.JSONField(default=list)
+    scientificName = models.CharField(max_length=255)
+    genus = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
