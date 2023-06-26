bb041eb813cc03e0c02adb895883cda2772fa6bd
Edouard Leurent <eleurent@gmail.com>
2023-06-26 22:08:37 +0100

    Style home page

--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -1,23 +1,114 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../authContext';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Button, ImageBackground, StyleSheet } from 'react-native';
 
 
 export default function HomeScreen({ navigation }) {
     const { authMethods } = useContext(AuthContext);
+    navigation.setOptions({
+        headerShown: true,
+        headerTransparent: true,
+    });
 
     return (
-        <View>
-            <Button
-                title="Botany"
-                onPress={() =>
-                    navigation.navigate('SpeciesList')
-                }
-            />
-            <Button
-                title="Sign out"
-                onPress={authMethods.signOut}
-            />
+        <View style={styles.container} >
+            <ImageBackground source={require('../assets/images/page-background.png')} style={styles.containerImage}>
+                <View>
+                    <View style={styles.categoryRowContainer}>
+                        <View style={{ flex: 1 }}></View>
+                        <TouchableOpacity
+                            style = {styles.categoryContainer}
+                            activeOpacity={0.5}
+                            onPress={() =>
+                                navigation.navigate('SpeciesList')
+                            }>
+                            <Image style={styles.categoryImage}
+                                source={require('../assets/images/botany.png')}
+                            />
+                            <Text style = {styles.categoryLabel}>BOTANY</Text>
+                        </TouchableOpacity>
+                        <View style={{ flex: 1 }}></View>
+                    </View>
+                    <View style={styles.categoryRowContainer}>
+                        <TouchableOpacity
+                            style={[styles.categoryContainer, { 'opacity': 0.3 }]}
+                            activeOpacity={0.3}>
+                            <Image style={styles.categoryImage}
+                                source={require('../assets/images/entomology.png')}
+                            />
+                            <Text style={styles.categoryLabel}>ENTOMOLOGY</Text>
+                        </TouchableOpacity>
+                        <View style={{ flex: 1 }}></View>
+                        <TouchableOpacity
+                            style={[styles.categoryContainer, {'opacity': 0.3}]}
+                            activeOpacity={0.3}>
+                            <Image style={styles.categoryImage}
+                                source={require('../assets/images/ornithology.png')}
+                            />
+                            <Text style={styles.categoryLabel}>ORNITHOLOGY</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.categoryRowContainer}>
+                        <View style={{ flex: 1 }}></View>
+                        <TouchableOpacity
+                            style={styles.categoryContainer}
+                            activeOpacity={0.5}
+                            onPress={() =>
+                                navigation.navigate('SpeciesList')
+                            }>
+                            <Image style={styles.categoryImage}
+                                source={require('../assets/images/university.png')}
+                            />
+                            <Text style={styles.categoryLabel}>UNIVERSITY</Text>
+                        </TouchableOpacity>
+                        <View style={{ flex: 1 }}></View>
+                    </View>
+                </View>
+                {/* <Button title="Sign out" onPress={authMethods.signOut} /> */}
+            </ImageBackground>
         </View>
     );
 };
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    containerImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+    categoryRowContainer: {
+        height: 120,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    categoryContainer: {
+        width: 110,
+        height: 120,
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+        justifyContent: 'center', 
+    },
+    categoryImage: {
+        width: 100,
+        height: 100,
+    },
+    categoryLabel: {
+        fontFamily: 'Old Standard TT',
+        fontStyle: 'normal',
+        fontWeight: 400,
+        fontSize: 14.3846,
+        lineHeight: 18,
+        display: 'flex',
+        alignItems: 'center',
+        textAlign: 'center',
+        color: '000000',
+    },
+});
--- frontend/screens/SpeciesList.js
+++ frontend/screens/SpeciesList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, ImageBackground, StyleSheet } from 'react-native';
 import axios from 'axios';
 
 const SPECIES_LIST_URL = 'http://localhost:8000/api/species/'
@@ -18,13 +18,35 @@ export default function SpeciesListScreen({ navigation, route }) {
         fetchSpeciesList();
     }, []);
 
+    navigation.setOptions({
+        headerShown: true,
+        headerTransparent: true,
+    });
+
+
     return (
-        <>{
+        <View style={styles.container} >
+        <ImageBackground source={require('../assets/images/page-background.png')} style={styles.containerImage}>
+        {
             speciesList.map((data) => {
                 return (
                     <Button key={data.id} title={data.name} onPress={() => navigation.navigate('SpeciesDetail', { id: data.id })} />
                 );
             })
-        }</>
+        }
+        </ImageBackground>
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    containerImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+});
