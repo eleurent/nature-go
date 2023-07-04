eaeefb60c37ad281d512af00ce9f139420d771ba
Edouard Leurent <eleurent@gmail.com>
2023-07-04 22:21:47 +0000

    Cache images

--- frontend/App.js
+++ frontend/App.js
@@ -1,23 +1,65 @@
-import React, { useState, useContext } from 'react'; 
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text, TextInput, Button } from 'react-native';
-// import * as React from 'react';
 import { useAuth, AuthContext } from './authContext';
 import HomeScreen from './screens/Home'
 import SignInScreen from './screens/Login'
 import SplashScreen from './screens/Splash';
 import SpeciesListScreen from './screens/SpeciesList';
 import SpeciesDetailScreen from './screens/SpeciesDetail';
+// import * as SplashScreen from 'expo-splash-screen';
+import { Asset } from 'expo-asset';
 
 
 const Stack = createNativeStackNavigator();
 
 
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
+
+
 export default function App() {
 
+  const [cacheIsReady, setCacheIsReady] = useState(false);
   const { authState, authMethods } = useAuth();
+
+
+  // Load any resources or data that you need prior to rendering the app
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        // SplashScreen.preventAutoHideAsync();
+        const imageAssets = cacheImages([
+          require('./assets/images/page-background.png'),
+        ]);
+        const fontAssets = cacheFonts([]);
+
+        await Promise.all([...imageAssets, ...fontAssets]);
+      } catch (e) {
+        // You might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setCacheIsReady(true);
+        // SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+
 
   return (
     <AuthContext.Provider value = {{ authState, authMethods }}>
