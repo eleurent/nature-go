6d1aff832f22f4657212c9e4c4979cba4fb03cdd
Edouard Leurent <eleurent@gmail.com>
2023-07-15 16:25:08 +0100

    Add views to get or create quizzes

--- backend/nature_go/university/urls.py
+++ backend/nature_go/university/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import QuizCreateView, QuizRetrieveView
+from .views import QuizCreateView, QuizRetrieveView, QuizGetOrCreateView
 
 urlpatterns = [
     path('quiz/create/', QuizCreateView.as_view(), name='quiz-create'),
+    path('quiz/get_or_create/', QuizGetOrCreateView.as_view(), name='quiz-get-or-create'),
     path('quiz/<int:pk>/', QuizRetrieveView.as_view(), name='quiz-retrieve'),
 ]
--- backend/nature_go/university/views.py
+++ backend/nature_go/university/views.py
@@ -3,6 +3,15 @@ from .models import MultipleChoiceQuestion, Quiz, MultipleChoiceUserAnswer
 from .serializers import MultipleChoiceQuestionSerializer, QuizSerializer, MultipleChoiceUserAnswerSerializer
 import random
 from django.utils import timezone
+from datetime import timedelta
+from django.shortcuts import redirect
+
+
+class QuizRetrieveView(generics.RetrieveAPIView):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 
 class QuizCreateView(generics.CreateAPIView):
     queryset = Quiz.objects.all()
@@ -10,34 +19,24 @@ class QuizCreateView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
-        # sample 10 random questions from all available questions
         questions = random.sample(list(MultipleChoiceQuestion.objects.all()), 2)
-        # set the user to the request user and the datetime to now
         serializer.save(user=self.request.user, multiple_choice_questions=questions)
+        
+    def get(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-    # def create(self, request, *args, **kwargs):
-    #     serializer = QuizSerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     observation = serializer.save(user=self.request.user)
-    #     result = plantnet_identify(observation.image.path)
-    #     species_data = result['results'][0]['species']
-    #     species, created = Species.objects.get_or_create(
-    #         name=species_data['scientificNameWithoutAuthor'],
-    #         commonNames=species_data['commonNames'],
-    #         scientificName=species_data['scientificNameWithoutAuthor'],
-    #         genus=species_data['genus']['scientificNameWithoutAuthor'],
-    #         family=species_data['family']['scientificNameWithoutAuthor'],
-    #     )
-    #     observation.species = species
-    #     observation.save()
-    #     serializer = ObservationSerializer(instance=observation)
-    #     return Response(serializer.data)
-
-
-
-
-class QuizRetrieveView(generics.RetrieveAPIView):
+class QuizGetOrCreateView(generics.RetrieveAPIView):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        recent_quizzes = Quiz.objects.filter(
+            user=self.request.user, datetime__gte=timezone.now() - timedelta(hours=24))
+        for quiz in recent_quizzes:
+            if not MultipleChoiceUserAnswer.objects.filter(quiz=quiz).exists():
+                return quiz
+        data = QuizCreateView.as_view()(self.request._request).data
+        return Quiz.objects.get(pk=data['id'])
+
