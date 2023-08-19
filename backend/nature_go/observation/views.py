47066386d88667207b601a41ef8225af60b94db2
Edouard Leurent <eleurent@gmail.com>
2023-08-19 12:52:42 +0100

    Handle image with no exif

--- backend/nature_go/observation/views.py
+++ backend/nature_go/observation/views.py
@@ -1,3 +1,4 @@
+import logging
 import requests
 from rest_framework import generics
 from rest_framework.response import Response
@@ -5,6 +6,8 @@ from rest_framework.parsers import MultiPartParser
 
 from observation.models import Species, Observation
 from observation.serializers import ObservationSerializer, SpeciesSerializer
+
+logger = logging.getLogger(__name__)
 
 
 class SpeciesList(generics.ListAPIView):
@@ -38,14 +41,23 @@ def read_exif(image_path):
     import PIL.Image
     import PIL.ExifTags
     import datetime
+    location = {}
+    datetime_ = ''
     img = PIL.Image.open(image_path)
-    exif = {PIL.ExifTags.TAGS[k]: v
-            for k, v in img._getexif().items() if k in PIL.ExifTags.TAGS}
-    location = {PIL.ExifTags.GPSTAGS.get(key,key): str(exif['GPSInfo'][key])
-                for key in exif.get('GPSInfo', {})}
-    datetime_ = exif.get('DateTime', '')
+    exif = img._getexif()
+    if not exif:
+        logger.error('No exif found.')
+    else:
+        exif = {PIL.ExifTags.TAGS[k]: v
+                for k, v in exif.items() if k in PIL.ExifTags.TAGS}
+        location = {PIL.ExifTags.GPSTAGS.get(key,key): str(exif['GPSInfo'][key])
+                    for key in exif.get('GPSInfo', {})}
+        datetime_ = exif.get('DateTime', '')
     if datetime_:
         datetime_ = datetime.datetime.strptime(datetime_, '%Y:%m:%d %H:%M:%S')
+    else:
+        logger.error('Found no datetime, setting it to now.')
+        datetime_ = datetime.datetime.now()
     return location, datetime_
 
 def plantnet_identify(image_path):
@@ -74,6 +86,7 @@ class ObservationCreate(generics.CreateAPIView):
         observation = serializer.save(user=self.request.user)
         result = plantnet_identify(observation.image.path)
         species_data = result['results'][0]['species']
+        # TODO: Get based on scientificName(pk), other fields set as default for creation only
         species, created = Species.objects.get_or_create(
             name=species_data['scientificNameWithoutAuthor'],
             commonNames=species_data['commonNames'],
