e1221ebdfb58904146ac53b4abce70b1dc3d5d09
nathangrinsztajn <ngrinsztajn@gmail.com>
2023-08-26 20:10:23 +0200

    better confirmation screen, closes #25

--- frontend/screens/ObservationConfirm.js
+++ frontend/screens/ObservationConfirm.js
@@ -6,7 +6,8 @@ import { FlatList } from 'react-native-gesture-handler';
 
 const API_URL = Constants.expoConfig.extra.API_URL;
 const OBSERVATION_URL = (id) => API_URL + `api/species/observation/${id}/`
-
+const NUM_CANDIDATES = 3; // Number of candidates to display
+const PROBABILITY_THRESHOLD = 0.25; // Minimum probability to display a candidate
 
 
 const SpeciesCandidate = (props) => {
@@ -16,7 +17,10 @@ const SpeciesCandidate = (props) => {
                 resizeMode='contain'
                 source={{ uri: props.item.images[0].url.s }}
             />
-            <Text style={styles.speciesScientificName}>{props.item.species.scientificName}</Text>
+            <View style={styles.textContainer}>
+                <Text style={styles.speciesName}>{props.item.species.commonNames[0]}</Text>
+                <Text style={styles.speciesScientificName}>{props.item.species.scientificNameWithoutAuthor}</Text>
+            </View>
             <TouchableOpacity
                 style={styles.validateButton}
                 activeOpacity={0.5}
@@ -42,27 +46,42 @@ const confirmSpeciesAsync = async (observation_id, species_index, navigation) =>
 export default function ObservationConfirmScreen({ navigation, route }) {
 
     const [observation, setObservation] = useState(null);
+    const [imageURL, setImageURL] = useState(null);
 
     useEffect(() => {
         const fetchObservation = async () => {
             const response = await axios.get(OBSERVATION_URL(route.params.id));
             console.log(response.data);
+            console.log(response.data.image);
             setObservation(response.data);
+            setImageURL(response.data.image);
         };
 
         fetchObservation();
     }, []);
 
     let has_results = observation && observation.identification_response && observation.identification_response.results;
+    let topNResults = has_results 
+    ? observation.identification_response.results
+        .filter(candidate => candidate.score >= PROBABILITY_THRESHOLD)
+        .slice(0, NUM_CANDIDATES) 
+    : [];
+    console.log(topNResults);
     return (
         <View style={styles.container}>
             <ImageBackground source={require('../assets/images/page-background.png')} style={styles.containerImage}>
+                {imageURL && (
+                    <Image
+                        style={styles.coverImage}
+                        source={{ uri: imageURL }}
+                    />
+                )}
                 {has_results ? <FlatList
                     style={{ marginTop: 60 }}
                     vertical
                     numColumns={1}
                     showsVerticalScrollIndicator={Platform.OS === 'web'}
-                    data={observation.identification_response.results}
+                    data={topNResults}
                     contentContainerStyle={{}}
                     renderItem={({ item, index }) => {
                         return (
@@ -79,39 +98,51 @@ export default function ObservationConfirmScreen({ navigation, route }) {
 
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
+    container: { flex: 1, flexDirection: 'column' },
+    containerImage: { flex: 1, resizeMode: 'cover' },
+    coverImage: {
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover'
     },
-    containerImage: {
-        flex: 1,
-        resizeMode: 'cover',
+    candidateContainer: { 
+        flex: 1, 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        marginVertical: 10, 
+        marginHorizontal: 10 
     },
-    candidateContainer: {
+    textContainer: {
         flex: 1,
-        flexDirection: 'row'
+        marginLeft: 8,
+        justifyContent: 'space-between' // Added this to distribute vertical space
     },
-    candidateImage: {
-        width: 64,
-        height: 64
+    candidateImage: { 
+        width: 100, 
+        height: 100, 
+        borderRadius: 50 
     },
     speciesName: {
         fontFamily: 'SpecialElite_400Regular',
         fontWeight: 400,
         fontSize: 20,
         color: '#331100',
-        opacity: 0.9
+        opacity: 0.9,
+        marginBottom: 10,
     },
-    speciesScientificName: {
-        fontFamily: 'Tinos_400Regular_Italic',
-        fontSize: 12,
+    speciesScientificName: { 
+        fontFamily: 'Tinos_400Regular_Italic', 
+        fontSize: 14, 
         color: '#332200',
-        opacity: 0.7
+        marginTop: 10
     },
-    validateButton: {
-        marginLeft: 10,
-        borderWidth: 1,
-        borderColor: "#ababab",
-        borderRadius: 18,
+    validateButton: { 
+        width: 80, 
+        paddingVertical: 5, 
+        paddingHorizontal: 10, 
+        borderRadius: 10, 
+        backgroundColor: '#4CAF50', 
+        alignItems: 'center' 
     },
+    categoryLabel: { color: 'white' }
 });
