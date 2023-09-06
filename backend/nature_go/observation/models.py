165e79ee06330e20e84e1e6ede4a97e75ca3a2ad
Edouard Leurent <eleurent@gmail.com>
2023-09-06 19:50:15 +0100

    Add occurences_cdf to species model

--- backend/nature_go/observation/models.py
+++ backend/nature_go/observation/models.py
@@ -8,12 +8,11 @@ from django.contrib.staticfiles import finders
 class Species(models.Model):
     commonNames = models.JSONField(default=list, blank=True)
     scientificName = models.CharField(max_length=255)
-    scientificNameWithoutAuthor = models.CharField(max_length=255)
+    scientificNameWithoutAuthor = models.CharField(max_length=255, unique=True)
     genus = models.CharField(max_length=255)
     family = models.CharField(max_length=255)
-
+    occurences_cdf = models.FloatField(null=True, blank=True)
     descriptions = models.JSONField(default=list, blank=True)
-
     illustration = models.ImageField(upload_to='species/illustration', blank=True)
     illustration_transparent = models.ImageField(upload_to='species/illustration_transparent', blank=True)
     wikipedia_image_url = models.URLField(max_length=255, blank=True)
@@ -28,8 +27,8 @@ class Observation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='observation/image')
     species = models.ForeignKey(Species, on_delete=models.CASCADE, blank=True, null=True)
-    location = models.JSONField(default=dict, blank=True)
-    identification_response = models.JSONField(default=dict, blank=True)
+    location = models.JSONField(default=dict, blank=True, null=True)
+    identification_response = models.JSONField(default=dict, blank=True, null=True)
     datetime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
