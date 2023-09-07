dada07cd78b70e2bf51d46bfc40aad22605af673
Edouard Leurent <eleurent@gmail.com>
2023-09-07 21:24:39 +0100

    Add limit to csv parse command

--- backend/nature_go/observation/management/commands/create_species_from_csv.py
+++ backend/nature_go/observation/management/commands/create_species_from_csv.py
@@ -7,12 +7,14 @@ class Command(BaseCommand):
     help = 'Load a species csv file into the database'
 
     def add_arguments(self, parser):
-        parser.add_argument('--path', type=str)
+        parser.add_argument('--path', type=str, default='../generation/data/plantnet_species.csv')
+        parser.add_argument('--limit', type=int, default=-1)
 
     def handle(self, *args, **kwargs):
         df = pd.read_csv(kwargs['path'])
-        df.sort_values(by='cdfOccurences', ascending=False)
-        df = df.iloc[:1000]
+        if kwargs['limit'] > 0:
+            df.sort_values(by='cdfOccurences', ascending=False)
+            df = df.iloc[:kwargs['limit']]
         species_create = []
         for row in df.itertuples():
             if Species.objects.filter(scientificName=row.scientificName).exists():
@@ -25,7 +27,11 @@ class Command(BaseCommand):
                 occurences_cdf=row.cdfOccurences,
             )
             species_create.append(species)
-        self.stdout.write(f'Will create {len(species_create)} species out of {df.shape[0]} datapoints')
+        self.stdout.write(f'Will create {len(species_create)} species out of {df.shape[0]} datapoints. Continue? [Y/n]')
+        yes = set(['yes', 'y', 'Y', ''])
+        choice = input().lower()
+        if choice not in yes:
+            return False
         Species.objects.bulk_create(species_create)
         self.stdout.write(
             self.style.SUCCESS(f'Successfully created {len(species_create)} species')
