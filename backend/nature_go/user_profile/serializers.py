12a00a8e67dfdc64d02e3ad62e5932ed01b4c1bf
Edouard Leurent <eleurent@gmail.com>
2023-09-10 16:27:26 +0100

    Add species and observations count to Profile

--- backend/nature_go/user_profile/serializers.py
+++ backend/nature_go/user_profile/serializers.py
@@ -6,9 +6,20 @@ class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username', read_only=True)
     current_level_xp = serializers.SerializerMethodField()
     next_level_xp = serializers.SerializerMethodField()
+    species_count = serializers.SerializerMethodField()
+    observations_count = serializers.SerializerMethodField()
     class Meta:
         model = Profile
-        fields = ['username', 'xp', 'level', 'content_unlocked', 'current_level_xp', 'next_level_xp']
+        fields = [
+             'username', 
+             'xp', 
+             'level', 
+             'content_unlocked', 
+             'current_level_xp', 
+             'next_level_xp',
+             'species_count',
+             'observations_count',
+        ]
         lookup_field = 'username'
 
     def get_current_level_xp(self, instance):
@@ -16,3 +27,9 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     def get_next_level_xp(self, instance):
          return instance.level_xp(instance.level + 1)
+    
+    def get_species_count(self, instance):
+        return len(set(obs.species for obs in instance.user.observation_set.all()))
+
+    def get_observations_count(self, instance):
+        return instance.user.observation_set.count()
