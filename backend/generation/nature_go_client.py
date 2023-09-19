83769c6dcbc74f2c68bab54ffee0ed43109bb643
Edouard Leurent <eleurent@gmail.com>
2023-09-19 21:36:41 +0100

    Update illustration generation workflows

--- backend/generation/nature_go_client.py
+++ backend/generation/nature_go_client.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 BASE_URL = 'http://nature-go.edouardleurent.com'
 LOGIN_URL = BASE_URL + '/api/auth/login/'
 SPECIES_LIST_URL = BASE_URL + '/api/species/all/'
+SPECIES_LABELED_URL = BASE_URL + '/api/species/labeled/'
 SPECIES_DETAIL_URL = BASE_URL + '/api/species/{species_id}/'
 QUESTION_CREATE_URL = BASE_URL + '/api/university/quiz/questions/'
 
@@ -27,10 +28,13 @@ class NatureGoClient:
             self.token = response.json()['token']
         else:
             raise ValueError(f'Login failed with status  {response.status_code} and response {response.json()}')
-
-    def get_all_species(self, limit=100, offset=0, ordering='-occurences_cdf') -> list:
+        
+    def assert_logged_in(self):
         if self.token is None:
             raise ValueError('Client is not logged in')
+
+    def get_all_species(self, limit=100, offset=0, ordering='-occurences_cdf') -> list:
+        self.assert_logged_in()
         params = {'limit': limit, 'offset': offset, 'ordering': ordering}
         response = requests.get(SPECIES_LIST_URL, headers={'Authorization': f'Token {self.token}'}, params=params)
         if response.status_code == 200:
@@ -40,23 +44,49 @@ class NatureGoClient:
         else:
             raise ValueError(f'{SPECIES_LIST_URL} failed with status  {response.status_code} and response {response.json()}')
 
-    def update_species_illustration(self, species_id: int, image: Image, illustration_name: str = 'illustration'):
-        if self.token is None:
-            raise ValueError('Client is not logged in')
+    def get_labeled_species(
+            self,
+            limit: int = 100, 
+            offset: int = 0, 
+            illustration: bool | None = None,
+            illustration_transparent: bool | None = None,
+            descriptions: bool | None = None,
+            multiplechoicequestions: bool | None = None,
+        ) -> list:
+        self.assert_logged_in()
+        params = {
+            'limit': limit,
+            'offset': offset,
+            'illustration': illustration,
+            'illustration_transparent': illustration_transparent,
+            'descriptions': descriptions,
+            'multiplechoicequestions': multiplechoicequestions,
+        }
+        params = {k: v for k, v in params.items() if v is not None}
+        response = requests.get(SPECIES_LABELED_URL, headers={'Authorization': f'Token {self.token}'}, params=params)
+        if response.status_code == 200:
+            result = response.json()
+            logger.info(f'Fetched {len(result)} species')
+            return result['results']
+        else:
+            raise ValueError(f'{SPECIES_LABELED_URL} failed with status  {response.status_code} and response {response.json()}')
+
+
+    def update_species_image(self, species_id: int, image: Image, image_name: str = 'illustration'):
+        self.assert_logged_in()
         buffered = io.BytesIO()
-        image.save(buffered, format="JPEG")
+        image.save(buffered, format="PNG")
         image_base64 = base64.b64encode(buffered.getvalue())
         response = requests.patch(SPECIES_DETAIL_URL.format(species_id=species_id),
                                   headers={'Authorization': f'Token {self.token}'},
-                                  data={illustration_name: image_base64})
+                                  data={image_name: image_base64})
         if response.status_code == 200:
             logger.info(f'Updated illustration for species {species_id}')
         else:
             raise ValueError(f'{SPECIES_DETAIL_URL} failed with status  {response.status_code} and response {response.content}')
         
     def get_all_questions(self) -> list:
-        if self.token is None:
-            raise ValueError('Client is not logged in')
+        self.assert_logged_in()
         response = requests.get(QUESTION_CREATE_URL, headers={'Authorization': f'Token {self.token}'})
         if response.status_code == 200:
             result = response.json()
@@ -67,8 +97,7 @@ class NatureGoClient:
 
         
     def post_species_questions(self, species_id: int, questions: list):
-        if self.token is None:
-            raise ValueError('Client is not logged in')
+        self.assert_logged_in()
         
         for question in questions:
             response = requests.post(QUESTION_CREATE_URL,
@@ -82,8 +111,7 @@ class NatureGoClient:
             raise ValueError(f'{QUESTION_CREATE_URL} failed with status  {response.status_code} and response {response.content}')
         
     def post_species_descriptions(self, species_id: int, descriptions: dict):
-        if self.token is None:
-            raise ValueError('Client is not logged in')
+        self.assert_logged_in()
         
         summaries = [
             descriptions['short_summary'],
