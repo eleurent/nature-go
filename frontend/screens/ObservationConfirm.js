f26e8ec9a1549529491d3a298768aa2a96def696
Edouard Leurent <eleurent@gmail.com>
2023-10-14 19:04:08 +0100

    Add an observation confirm screen

--- frontend/screens/ObservationConfirm.js
+++ frontend/screens/ObservationConfirm.js
@@ -1,159 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, ImageBackground, Platform, TouchableOpacity , ActivityIndicator} from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { View, Text, Image, StyleSheet, ImageBackground, Platform, TouchableOpacity , ActivityIndicator, ScrollView} from 'react-native';
 import axios from 'axios';
 import Constants from 'expo-constants'
 import { FlatList } from 'react-native-gesture-handler';
-import XPModal from '../components/XPModal';
-import { CommonActions } from '@react-navigation/native';
+import { ObservationContext } from '../contexts/ObservationContext';
+import MapView, { Marker } from '../components/CustomMapView';
+import { Ionicons } from '@expo/vector-icons';
 
 const API_URL = Constants.expoConfig.extra.API_URL;
-const URL_CREATE_OBSERVATION = API_URL + 'api/species/observation/'
-const OBSERVATION_URL = (id) => API_URL + `api/species/observation/${id}/`
-const NUM_CANDIDATES = 3; // Number of candidates to display
-const PROBABILITY_THRESHOLD = 0.10; // Minimum probability to display a candidate
 
 
-const SpeciesCandidate = (props) => {
+
+const ImageType = ({ selected, onSelect, name, text }) => {
+    const color = selected ? 'green' : 'gray';
     return (
-        <View style={styles.candidateContainer}>
-            <Image style={styles.candidateImage}
-                resizeMode='contain'
-                source={{ uri: props.item.images[0].url.s }}
-            />
-            <View style={styles.textContainer}>
-                <Text style={styles.speciesName}>{props.item.species.commonNames.size ? props.item.species.commonNames[0] : props.item.species.scientificNameWithoutAuthor}</Text>
-                <Text style={styles.speciesScientificName}>{props.item.species.scientificNameWithoutAuthor}</Text>
+        <TouchableOpacity onPress={onSelect} style={{marginLeft: 20}}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Ionicons
+                    name={ name }
+                    size={32}
+                    color={color}
+                />
+                <Text style={{ fontSize: 24, marginLeft: 5, color: color }}>{text}</Text>
             </View>
-            <TouchableOpacity
-                style={styles.validateButton}
-                activeOpacity={0.5}
-                onPress={props.onPress}>
-                <Text style={styles.categoryLabel}>Validate</Text>
-            </TouchableOpacity>
-        </View>
+        </TouchableOpacity>
     );
-}
-
-const postObservationImageAsync = async (imageBase64, gpsLocation, datetime, observation, setObservation, setIsLoading) => {
-    if (imageBase64 && !observation) {
-        let formData = new FormData();
-        console.log('posting observation with gpsLocation ' + JSON.stringify(gpsLocation));
-        console.log('posting observation with datetime ' + JSON.stringify(datetime));
-        formData.append('image', imageBase64);
-        formData.append('location', JSON.stringify(gpsLocation));
-        formData.append('datetime', datetime);
-        axios.post(URL_CREATE_OBSERVATION, formData, {
-            headers: { 'Content-Type': 'multipart/form-data' }
-        }).then(response => {
-            setObservation(response.data);
-            setIsLoading(false);
-        }).catch(error => {
-            console.log(error.message);
-            setIsLoading(false);
-        })
-    }
-};
-
-const confirmSpeciesAsync = async (observation_id, species_index, onConfirmResponse) => {
-    let formData = new FormData();
-    formData.append('species', species_index);
-    const config = {
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        },
-        timeout: 10000 // 10 seconds
-    };
-    axios.patch(OBSERVATION_URL(observation_id), formData, config)
-        .then(response => {
-            onConfirmResponse(response);
-        })
-        .catch(error => console.log(error));
 };
 
 
-const goToSpeciesDetails = (navigation, observation) => {
-    navigation.dispatch((state) => {
-        //update navigation state as you want.
-        const routes = [
-            { name: 'Home' },
-            { name: 'SpeciesList' },
-            { name: 'SpeciesDetail', params: { id: observation.species } }, //you can also add params 
-        ];
-
-        return CommonActions.reset({
-            ...state,
-            routes,
-            index: routes.length - 1,
-        });
-    });
-}
 
 export default function ObservationConfirmScreen({ navigation, route }) {
-
-    const [observation, setObservation] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [xpModalVisible, setXPModalVisible] = useState(false);
-    const imageBase64 = route.params.imageBase64;
-    const gpsLocation = route.params.gpsLocation;
-    const datetime = route.params.datetime;
+    const [statusMessage, setStatusMessage] = useState(null);
+    const { observationState, observationMethods } = useContext(ObservationContext);
+    const [selectedImageType, setSelectedImageType] = useState('leaf');
 
     useEffect(() => {
-        if (!observation)
-            postObservationImageAsync(imageBase64, gpsLocation, datetime, observation, setObservation, setIsLoading);
+    
     }, []);
 
-    let has_results = observation && observation.identification_response && observation.identification_response.results;
-    let topNResults = has_results ? observation.identification_response.results.filter(candidate => candidate.score >= PROBABILITY_THRESHOLD).slice(0, NUM_CANDIDATES) : [];
-    let emptyResults = has_results && topNResults.length === 0;
-
-    const onConfirmResponse = (response) => { setObservation(response.data); setXPModalVisible(true); };
-
-    const onXPModalClose = () => {
-        setXPModalVisible(false);
-
-        // Timeout needed to prevent iOS crash, presumably from chaining modal and navigation transitions?
-        // see https://github.com/react-navigation/react-navigation/issues/11259
-        setTimeout(() => { goToSpeciesDetails(navigation, observation); }, 100);
-
-    };
-
+    let initialRegion = undefined;
+    let coordinate = undefined;
+    if (observationState?.location?.latitude) {
+        initialRegion = {
+            latitude: observationState?.location?.latitude,
+            longitude: observationState?.location?.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+        };
+        coordinate = observationState?.location;
+    }
+    
     return (
         <View style={styles.container}>
             <ImageBackground source={require('../assets/images/page-background.png')} style={styles.containerImage}>
-                {imageBase64 ? (
+                <ScrollView>
+                {observationState.image ? (
                     <Image
                         style={styles.coverImage}
-                        source={{ uri: `data:image/png;base64,${imageBase64}` }}
+                        source={{ uri: `data:image/png;base64,${observationState.image}` }}
                     />
                 ) : null}
-                { isLoading ? (
-                    <View style={styles.loadingContainer}>
-                        <ActivityIndicator size={80} color="#00ff00" />
-                        <Text>Identifying the plant...</Text>
+                {statusMessage ? (
+                    <View style={styles.statusMessageContainer}>
+                        <ActivityIndicator size={80} color="#fff" />
+                        <Text style={styles.statusMessage}>{statusMessage}</Text>
                     </View>
-                ) :
-                (has_results ?
-                    (!emptyResults ?  
-                    <FlatList
-                        style={{ marginTop: 60 }}
-                        vertical
-                        numColumns={1}
-                        showsVerticalScrollIndicator={Platform.OS === 'web'}
-                        data={topNResults}
-                        contentContainerStyle={{}}
-                        renderItem={({ item, index }) => {
-                            return (
-                                <SpeciesCandidate
-                                    key={index}
-                                    item={item}
-                                    onPress={() => confirmSpeciesAsync(observation.id, index, onConfirmResponse)}
-                                />
-                            );
-                        }}
+                ) : null}
+                <View style={{flexDirection: 'row', marginLeft: 'auto', marginRight: 'auto', marginTop: 20}}>
+                    <ImageType
+                        name="leaf"
+                        text="leaf"
+                        selected={selectedImageType === 'leaf'}
+                        onSelect={() => setSelectedImageType('leaf')}
                     />
-                    : <Text style={styles.emptyResults}>I don't recognize this specimen. Maybe I should get closer?</Text>)
-                : null)}
-                <XPModal isVisible={xpModalVisible} xpData={observation?.xp} onClose={onXPModalClose}/>
+                    <ImageType
+                        name="flower-outline"
+                        text="flower"
+                        selected={selectedImageType === 'flower'}
+                        onSelect={() => setSelectedImageType('flower')}
+                    />
+                </View>
+                <MapView
+                    style={styles.map}
+                    initialRegion={initialRegion}
+                    scrollEnabled={false}
+                    zoomEnabled={false}
+                >
+                    {coordinate ? <Marker coordinate={coordinate} /> : null}
+                </MapView>
+                <TouchableOpacity style={styles.button} onPress={() => {}}>
+                    <Text style={styles.buttonText}>Confirm</Text>
+                </TouchableOpacity>
+                </ScrollView>
             </ImageBackground>
         </View>
     );
@@ -165,15 +103,8 @@ const styles = StyleSheet.create({
     containerImage: { flex: 1, resizeMode: 'cover' },
     coverImage: {
         width: '100%',
-        height: 250,
+        height: 450,
         resizeMode: 'cover'
-    },
-    candidateContainer: { 
-        flex: 1, 
-        flexDirection: 'row', 
-        alignItems: 'center', 
-        marginVertical: 10, 
-        marginHorizontal: 10 
     },
     textContainer: {
         flex: 1,
@@ -186,41 +117,32 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         fontFamily: 'SpecialElite_400Regular'
     },
-    candidateImage: { 
-        width: 100, 
-        height: 100, 
-        borderRadius: 50 
+    statusMessageContainer: {
+        position: "absolute",
+        top: '40%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
     },
-    speciesName: {
-        fontFamily: 'SpecialElite_400Regular',
-        fontWeight: 400,
-        fontSize: 20,
-        color: '#331100',
-        opacity: 0.9,
-        marginBottom: 10,
-    },
-    speciesScientificName: { 
-        fontFamily: 'Tinos_400Regular_Italic', 
-        fontSize: 14, 
-        color: '#332200',
-        marginTop: 10
-    },
-    validateButton: { 
-        width: 80, 
-        paddingVertical: 5, 
-        paddingHorizontal: 10, 
-        borderRadius: 10, 
-        backgroundColor: '#4CAF50', 
-        alignItems: 'center' 
-    },
-    categoryLabel: { color: 'white' },
-    emptyResults: {
+    statusMessage: {
         fontSize: 24,
-        textAlign: 'center',
-        paddingLeft: 10,
-        paddingRight: 10,
-        marginTop: 'auto',
-        marginBottom: 'auto',
-        fontFamily: 'Tinos_400Regular'
+        fontFamily: 'Tinos_400Regular',
+        color: 'white',
     },
+    map: {
+        marginTop: 20,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: 256,
+        height: 200,
+    },
+    button: {
+        marginTop: 20,
+        marginBottom: 20,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    buttonText: {
+        fontSize: 24,
+        fontFamily: 'Tinos_400Regular',
+    }
 });
---
+++ frontend/screens/ObservationSelect.js
@@ -0,0 +1,226 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, ImageBackground, Platform, TouchableOpacity , ActivityIndicator} from 'react-native';
+import axios from 'axios';
+import Constants from 'expo-constants'
+import { FlatList } from 'react-native-gesture-handler';
+import XPModal from '../components/XPModal';
+import { CommonActions } from '@react-navigation/native';
+
+const API_URL = Constants.expoConfig.extra.API_URL;
+const URL_CREATE_OBSERVATION = API_URL + 'api/species/observation/'
+const OBSERVATION_URL = (id) => API_URL + `api/species/observation/${id}/`
+const NUM_CANDIDATES = 3; // Number of candidates to display
+const PROBABILITY_THRESHOLD = 0.10; // Minimum probability to display a candidate
+
+
+const SpeciesCandidate = (props) => {
+    return (
+        <View style={styles.candidateContainer}>
+            <Image style={styles.candidateImage}
+                resizeMode='contain'
+                source={{ uri: props.item.images[0].url.s }}
+            />
+            <View style={styles.textContainer}>
+                <Text style={styles.speciesName}>{props.item.species.commonNames.size ? props.item.species.commonNames[0] : props.item.species.scientificNameWithoutAuthor}</Text>
+                <Text style={styles.speciesScientificName}>{props.item.species.scientificNameWithoutAuthor}</Text>
+            </View>
+            <TouchableOpacity
+                style={styles.validateButton}
+                activeOpacity={0.5}
+                onPress={props.onPress}>
+                <Text style={styles.categoryLabel}>Validate</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const postObservationImageAsync = async (imageBase64, gpsLocation, datetime, observation, setObservation, setIsLoading) => {
+    if (imageBase64 && !observation) {
+        let formData = new FormData();
+        console.log('posting observation with gpsLocation ' + JSON.stringify(gpsLocation));
+        console.log('posting observation with datetime ' + JSON.stringify(datetime));
+        formData.append('image', imageBase64);
+        formData.append('location', JSON.stringify(gpsLocation));
+        formData.append('datetime', datetime);
+        axios.post(URL_CREATE_OBSERVATION, formData, {
+            headers: { 'Content-Type': 'multipart/form-data' }
+        }).then(response => {
+            setObservation(response.data);
+            setIsLoading(false);
+        }).catch(error => {
+            console.log(error.message);
+            setIsLoading(false);
+        })
+    }
+};
+
+const confirmSpeciesAsync = async (observation_id, species_index, onConfirmResponse) => {
+    let formData = new FormData();
+    formData.append('species', species_index);
+    const config = {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        timeout: 10000 // 10 seconds
+    };
+    axios.patch(OBSERVATION_URL(observation_id), formData, config)
+        .then(response => {
+            onConfirmResponse(response);
+        })
+        .catch(error => console.log(error));
+};
+
+
+const goToSpeciesDetails = (navigation, observation) => {
+    navigation.dispatch((state) => {
+        //update navigation state as you want.
+        const routes = [
+            { name: 'Home' },
+            { name: 'SpeciesList' },
+            { name: 'SpeciesDetail', params: { id: observation.species } }, //you can also add params 
+        ];
+
+        return CommonActions.reset({
+            ...state,
+            routes,
+            index: routes.length - 1,
+        });
+    });
+}
+
+export default function ObservationSelectScreen({ navigation, route }) {
+
+    const [observation, setObservation] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [xpModalVisible, setXPModalVisible] = useState(false);
+    const imageBase64 = route.params.imageBase64;
+    const gpsLocation = route.params.gpsLocation;
+    const datetime = route.params.datetime;
+
+    useEffect(() => {
+        if (!observation)
+            postObservationImageAsync(imageBase64, gpsLocation, datetime, observation, setObservation, setIsLoading);
+    }, []);
+
+    let has_results = observation && observation.identification_response && observation.identification_response.results;
+    let topNResults = has_results ? observation.identification_response.results.filter(candidate => candidate.score >= PROBABILITY_THRESHOLD).slice(0, NUM_CANDIDATES) : [];
+    let emptyResults = has_results && topNResults.length === 0;
+
+    const onConfirmResponse = (response) => { setObservation(response.data); setXPModalVisible(true); };
+
+    const onXPModalClose = () => {
+        setXPModalVisible(false);
+
+        // Timeout needed to prevent iOS crash, presumably from chaining modal and navigation transitions?
+        // see https://github.com/react-navigation/react-navigation/issues/11259
+        setTimeout(() => { goToSpeciesDetails(navigation, observation); }, 100);
+
+    };
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={require('../assets/images/page-background.png')} style={styles.containerImage}>
+                {imageBase64 ? (
+                    <Image
+                        style={styles.coverImage}
+                        source={{ uri: `data:image/png;base64,${imageBase64}` }}
+                    />
+                ) : null}
+                { isLoading ? (
+                    <View style={styles.loadingContainer}>
+                        <ActivityIndicator size={80} color="#00ff00" />
+                        <Text>Identifying the plant...</Text>
+                    </View>
+                ) :
+                (has_results ?
+                    (!emptyResults ?  
+                    <FlatList
+                        style={{ marginTop: 60 }}
+                        vertical
+                        numColumns={1}
+                        showsVerticalScrollIndicator={Platform.OS === 'web'}
+                        data={topNResults}
+                        contentContainerStyle={{}}
+                        renderItem={({ item, index }) => {
+                            return (
+                                <SpeciesCandidate
+                                    key={index}
+                                    item={item}
+                                    onPress={() => confirmSpeciesAsync(observation.id, index, onConfirmResponse)}
+                                />
+                            );
+                        }}
+                    />
+                    : <Text style={styles.emptyResults}>I don't recognize this specimen. Maybe I should get closer?</Text>)
+                : null)}
+                <XPModal isVisible={xpModalVisible} xpData={observation?.xp} onClose={onXPModalClose}/>
+            </ImageBackground>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: { flex: 1, flexDirection: 'column' },
+    containerImage: { flex: 1, resizeMode: 'cover' },
+    coverImage: {
+        width: '100%',
+        height: 250,
+        resizeMode: 'cover'
+    },
+    candidateContainer: { 
+        flex: 1, 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        marginVertical: 10, 
+        marginHorizontal: 10 
+    },
+    textContainer: {
+        flex: 1,
+        marginLeft: 8,
+        justifyContent: 'space-between'
+    },
+    loadingContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontFamily: 'SpecialElite_400Regular'
+    },
+    candidateImage: { 
+        width: 100, 
+        height: 100, 
+        borderRadius: 50 
+    },
+    speciesName: {
+        fontFamily: 'SpecialElite_400Regular',
+        fontWeight: 400,
+        fontSize: 20,
+        color: '#331100',
+        opacity: 0.9,
+        marginBottom: 10,
+    },
+    speciesScientificName: { 
+        fontFamily: 'Tinos_400Regular_Italic', 
+        fontSize: 14, 
+        color: '#332200',
+        marginTop: 10
+    },
+    validateButton: { 
+        width: 80, 
+        paddingVertical: 5, 
+        paddingHorizontal: 10, 
+        borderRadius: 10, 
+        backgroundColor: '#4CAF50', 
+        alignItems: 'center' 
+    },
+    categoryLabel: { color: 'white' },
+    emptyResults: {
+        fontSize: 24,
+        textAlign: 'center',
+        paddingLeft: 10,
+        paddingRight: 10,
+        marginTop: 'auto',
+        marginBottom: 'auto',
+        fontFamily: 'Tinos_400Regular'
+    },
+});
