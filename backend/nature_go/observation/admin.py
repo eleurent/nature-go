3155ca61beb576061c82ae5f2e0d11a6dcd9369b
Edouard Leurent <eleurent@gmail.com>
2023-10-15 13:47:22 +0100

    Searchable species in bakend admin

--- backend/nature_go/observation/admin.py
+++ backend/nature_go/observation/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Species, Observation
 
-admin.site.register(Species)
+
+class SpeciesAdmin(admin.ModelAdmin):
+    search_fields = ('scientificNameWithoutAuthor', 'commonNames')
+
+
+admin.site.register(Species, SpeciesAdmin)
 admin.site.register(Observation)
