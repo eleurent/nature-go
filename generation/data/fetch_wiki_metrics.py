51f8518c8bef08a2607ebc169087272e2e7b91ac
Edouard Leurent <eleurent@gmail.com>
2024-01-18 23:17:29 +0000

    Update bird species with word count

--- generation/data/fetch_wiki_metrics.py
+++ generation/data/fetch_wiki_metrics.py
@@ -11,6 +11,11 @@ import http.client
 
 wikipedia.set_lang('en')
 
+
+SPECIES_TYPE = 'bird'  # bird / plant
+SPECIES_NAME_FIELD = 'scientificName'
+FILENAME = 'bird_species.csv'
+
 def retry_request(max_retries=3, delay_range=(0.5, 1.5)):
     def decorator(func):
         @wraps(func)
@@ -29,29 +34,29 @@ def retry_request(max_retries=3, delay_range=(0.5, 1.5)):
     return decorator
 
 @retry_request(max_retries=3, delay_range=(0.5, 1.5))
-def get_wiki_word_count(plant_name):
+def get_wiki_word_count(species_name):
     try:
-        content = wikipedia.page(plant_name, auto_suggest=False).content
+        content = wikipedia.page(species_name, auto_suggest=False).content
         return len(content.split())
     except wikipedia.exceptions.PageError:
         return -1
     except wikipedia.exceptions.DisambiguationError:
         try:
-            content = wikipedia.page(f"{plant_name} (plant)", auto_suggest=False).content
+            content = wikipedia.page(f"{species_name} ({SPECIES_TYPE})", auto_suggest=False).content
             return len(content.split())
         except (wikipedia.exceptions.PageError, wikipedia.exceptions.DisambiguationError):
             return -1
 
 
 # Add a new column to the DataFrame with Wikipedia word counts
-df = pd.read_csv('species.csv')
+df = pd.read_csv(FILENAME)
 
 chunk_size = 100  # Number of rows to process before saving
 n_chunks = len(df) // chunk_size + (1 if len(df) % chunk_size else 0)
 
 def parallel_get_wiki_word_count(start_idx, end_idx):
     with ThreadPoolExecutor(max_workers=5) as executor:
-        results = list(executor.map(get_wiki_word_count, df.loc[start_idx:end_idx-1, 'scientificNameWithoutAuthor']))
+        results = list(executor.map(get_wiki_word_count, df.loc[start_idx:end_idx-1, SPECIES_NAME_FIELD]))
     return results
 
 for i in tqdm(range(n_chunks)):
@@ -62,4 +67,4 @@ for i in tqdm(range(n_chunks)):
     df.loc[start_idx:end_idx-1, 'WikipediaWordCount'] = parallel_get_wiki_word_count(start_idx, end_idx)
     
     # Save the DataFrame to the same file
-    df.to_csv('species_with_word_count.csv', index=False)
+    df.to_csv(FILENAME, index=False)
