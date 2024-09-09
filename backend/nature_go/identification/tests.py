040e2f8237a30a02136b4c38d4f55b89b541cbd9
Edouard Leurent <eleurent@gmail.com>
2024-09-09 13:33:31 +0000

    Add a draft identification test

---
+++ backend/nature_go/identification/tests.py
@@ -0,0 +1,28 @@
+
+from django.test import TestCase
+from identification import gemini
+from observation.models import Species
+
+
+class GeminiTestCase(TestCase):
+    def setUp(self):
+        Species.objects.create(
+            type=Species.BIRD_TYPE,
+            scientificNameWithoutAuthor='Alcedo atthis',
+            commonNames=['Common kingfisher'],
+            genus='Alcedo',
+            family='Alcedinidae',
+        )
+
+    def test_gemini_identification(self):
+        image_path = './identification/test_bird_image.jpg'  # TODO: download from link
+        location = {}
+
+        response = gemini.gemini_identify_few_shot(
+            image_path=image_path,
+            location=location,
+            few_shots=gemini.BIRD_ID_FEW_SHOTS,
+        )
+        print(response)
+        self.assertIsNotNone(response)
+
