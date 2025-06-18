4a0776ab7eff1c57161f99dd8e623393c293eab0
Edouard Leurent <eleurent@gmail.com>
2025-06-18 17:10:59 +0000

    Fix replicate input type to bytes

--- backend/nature_go/generation/replicate.py
+++ backend/nature_go/generation/replicate.py
@@ -1,12 +1,16 @@
 import replicate
+import io
 from PIL import Image
 
 BACKGROUND_REMOVAL_ENDPOINT = "abhisingh0909/rembg:9b5eeb79d1be3900b39303bbbc0b96dab38515cb06c92eac75425596cf753393"
 
 
 def remove_background(image: Image) -> Image:
+    raw_bytes = io.BytesIO()
+    image.save(raw_bytes, format='PNG')
+    raw_bytes = raw_bytes.getvalue()
     input = {
-        "image":  image
+        "image":  raw_bytes
     }
     output = replicate.run(
         BACKGROUND_REMOVAL_ENDPOINT,
