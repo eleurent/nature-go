8a2915a8cb3732367149670dcebddc79076bf3c8
Edouard Leurent <edouardl@google.com>
2026-01-31 07:30:46 +0000

    Fix CSRF 403: exempt auth endpoints from CSRF checks

--- backend/nature_go/authentication/urls.py
+++ backend/nature_go/authentication/urls.py
@@ -1,16 +1,15 @@
-from django.urls import path
 from authentication import views
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = format_suffix_patterns([
-    path('login/',
-        obtain_auth_token,
-        name='auth_user_login'),
-    path('register/',
-        views.CreateUserAPIView.as_view(),
-        name='auth_user_create'),
-    path('logout/',
-        views.LogoutUserAPIView.as_view(),
-        name='auth_user_logout')
+    path('login/', csrf_exempt(obtain_auth_token), name='auth_user_login'),
+    path(
+        'register/',
+        csrf_exempt(views.CreateUserAPIView.as_view()),
+        name='auth_user_create',
+    ),
+    path('logout/', views.LogoutUserAPIView.as_view(), name='auth_user_logout'),
 ])
