35fe2ab6676a245d81aed3c66c1cb88efb8eebf1
Edouard Leurent <edouardl@google.com>
2026-02-07 22:56:42 +0000

    Add /api/ root endpoint to show available API endpoints

--- backend/nature_go/nature_go/urls.py
+++ backend/nature_go/nature_go/urls.py
@@ -18,13 +18,29 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include
-from django.urls import path, re_path
+from django.urls import include, path, re_path
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from website.views import serve_pwa
+
+
+@api_view(['GET'])
+def api_root(request):
+  return Response({
+      'species': reverse('species-list', request=request),
+      'auth': request.build_absolute_uri('/api/auth/'),
+      'profile': request.build_absolute_uri('/api/profile/'),
+      'badge': request.build_absolute_uri('/api/badge/'),
+      'poster': request.build_absolute_uri('/api/poster/regions/'),
+      'university': request.build_absolute_uri('/api/university/'),
+  })
+
 
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
+    path('api/', api_root, name='api-root'),
     path('api/species/', include('observation.urls')),
     path('api/university/', include('university.urls')),
     path('api/auth/', include('authentication.urls')),
@@ -37,5 +53,5 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
