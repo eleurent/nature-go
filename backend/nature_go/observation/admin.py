6ad477ba6459bdbc5d21f38ed77bb0c14704ab82
Edouard Leurent <edouardl@google.com>
2026-02-14 14:29:03 +0000

    Add user filter to Observation admin interface

--- backend/nature_go/observation/admin.py
+++ backend/nature_go/observation/admin.py
@@ -1,55 +1,61 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import JsonResponse
 from django.core.paginator import Paginator
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
-from .models import Species, Observation
+from django.urls import path
+from .models import Observation, Species
 
 
 class SpeciesAdmin(admin.ModelAdmin):
-    search_fields = ('scientificNameWithoutAuthor', 'commonNames')
-    list_filter = ['type']
-    actions = ['delete_illustration']
+  search_fields = ('scientificNameWithoutAuthor', 'commonNames')
+  list_filter = ['type']
+  actions = ['delete_illustration']
 
-    def get_urls(self):
-        urls = super().get_urls()
-        custom_urls = [
-            path('illustration-review/', self.admin_site.admin_view(self.species_illustration_review), name='species_illustration_review'),
-        ]
-        return custom_urls + urls
+  def get_urls(self):
+    urls = super().get_urls()
+    custom_urls = [
+        path(
+            'illustration-review/',
+            self.admin_site.admin_view(self.species_illustration_review),
+            name='species_illustration_review',
+        ),
+    ]
+    return custom_urls + urls
 
-    def species_illustration_review(self, request):
-        """Visit /admin/observation/species/illustration-review/"""
-       
-        if request.POST.get('action', '') == 'delete_illustration':
-            queryset = Species.objects.filter(id=request.POST['id'])
-            self.delete_illustration(request=request, queryset=queryset)
-            
-        species = Species.objects.all()
-        type_filter = request.GET.get('type')
-        if type_filter:
-            species = species.filter(type=type_filter)
-        ordering = request.GET.get('ordering')
-        if ordering:
-            species = species.order_by(ordering)
-        paginator = Paginator(species, 1)
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        context = {
-            'page_obj': page_obj,
-        }
-        return render(request, 'admin/species_illustration_review.html', context)
+  def species_illustration_review(self, request):
+    """Visit /admin/observation/species/illustration-review/"""
 
-    def delete_illustration(self, request, queryset):
-        for species in queryset:
-            species.illustration.delete()
-            species.illustration_transparent.delete()
-            species.save()
+    if request.POST.get('action', '') == 'delete_illustration':
+      queryset = Species.objects.filter(id=request.POST['id'])
+      self.delete_illustration(request=request, queryset=queryset)
 
-    delete_illustration.short_description = 'Delete illustration'
+    species = Species.objects.all()
+    type_filter = request.GET.get('type')
+    if type_filter:
+      species = species.filter(type=type_filter)
+    ordering = request.GET.get('ordering')
+    if ordering:
+      species = species.order_by(ordering)
+    paginator = Paginator(species, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'admin/species_illustration_review.html', context)
+
+  def delete_illustration(self, request, queryset):
+    for species in queryset:
+      species.illustration.delete()
+      species.illustration_transparent.delete()
+      species.save()
+
+  delete_illustration.short_description = 'Delete illustration'
 
 
+class ObservationAdmin(admin.ModelAdmin):
+  list_filter = ['user']
 
 
 admin.site.register(Species, SpeciesAdmin)
-admin.site.register(Observation)
+admin.site.register(Observation, ObservationAdmin)
